69bc2b2166efad92a9492e10d1eb7bf24a9eb7f8
Miller Adulu <milleradulu@gmail.com>
2019-09-26 09:49:41 +0300

    Enable addition of patient records for  pre-defined patient

--- src/components/AddPatientRecord.vue
+++ src/components/AddPatientRecord.vue
@@ -1,45 +1,91 @@
 <template>
-    <v-card>
-      <v-card-title class="grey lighten-4 py-4 title">Add HomeSense</v-card-title>
-      <v-container grid-list-sm class="pa-4">
-        <v-layout row wrap>
-          
-          <v-flex xs12>
-            <v-text-field placeholder="Patient ID" v-model="patientId"/>
-          </v-flex>
-          <v-flex xs12>
-            <v-textarea prepend-icon="notes" name="Symptoms" hint="Symptoms" persistent-hint v-model="symptoms"></v-textarea>
-          </v-flex>
-          <v-flex xs12>
-            <v-textarea prepend-icon="notes" name="Diagnosis" hint="Diagnosis" persistent-hint v-model="diagnosis"></v-textarea>
-          </v-flex>
-          <v-flex xs12>
-            <v-textarea prepend-icon="notes" name="Treatment Plan" hint="Treatment Plan" persistent-hint v-model="treatmentPlan"></v-textarea>
-          </v-flex>
-          <v-flex>
-            <v-btn
-            :loading="loading"
-            block
-            color="green white--text"
-            @click="addPatientRecord">Add</v-btn>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-card>
+  <v-card>
+    <v-card-title class="grey lighten-4 py-4 title">Add HomeSense</v-card-title>
+    <v-container grid-list-sm class="pa-4">
+      <v-layout row wrap>
+        <!-- <v-flex xs12>
+          <v-text-field placeholder="Patient ID" v-model="patientId" />
+        </v-flex>-->
+        <v-flex xs12>
+          <v-textarea
+            prepend-icon="notes"
+            name="Symptoms"
+            hint="Symptoms"
+            persistent-hint
+            v-model="symptoms"
+          ></v-textarea>
+        </v-flex>
+        <v-flex xs12>
+          <v-textarea
+            prepend-icon="notes"
+            name="Diagnosis"
+            hint="Diagnosis"
+            persistent-hint
+            v-model="diagnosis"
+          ></v-textarea>
+        </v-flex>
+        <v-flex xs12>
+          <v-textarea
+            prepend-icon="notes"
+            name="Treatment Plan"
+            hint="Treatment Plan"
+            persistent-hint
+            v-model="treatmentPlan"
+          ></v-textarea>
+        </v-flex>
+        <v-flex x12>
+          <v-text-field
+            prepend-icon="notes"
+            label="Seen By"
+            name="seen_by"
+            hint="Dr. Adulu"
+            type="text"
+            v-model="seenBy"
+          ></v-text-field>
+        </v-flex>
+        <v-flex x12>
+          <v-btn :loading="loading" block color="green white--text" @click="addPatientRecord">Add</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-card>
 </template>
 
 <script>
+import apiClient from "@/plugins/api";
 export default {
-    name: 'AddPatientRecord',
-    data: () => ({
-        loading: false,
-        patientId: '',
-        symptoms: '',
-        diagnosis: '',
-        treatmentPlan: ''
-    }),
-    methods: {
-        addPatientRecord(){}
+  name: "AddPatientRecord",
+  data: () => ({
+    loading: false,
+    symptoms: "",
+    diagnosis: "",
+    treatmentPlan: "",
+    seenBy: ""
+  }),
+  methods: {
+    async addPatientRecord() {
+      this.initLoading();
+
+      apiClient
+        .patch(`/records/${33989061}`, {
+          symptoms: this.symptoms,
+          diagnosis: this.diagnosis,
+          treatment_plan: this.treatmentPlan,
+          seen_by: this.seenBy
+        })
+        .then(response => {
+          if (response.success) console.log("Success Papi");
+          else console.log("No success Papi!");
+
+          this.finishLoading();
+        });
+    },
+    initLoading() {
+      this.loading = true;
+    },
+    finishLoading() {
+      this.loading = false;
     }
-}
+  }
+};
 </script>
--- src/components/HealthFacilities.vue
+++ src/components/HealthFacilities.vue
@@ -1,46 +1,41 @@
 <template>
-<v-card>
+  <v-card>
     <v-card-title>
       Health Facilities
       <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        append-icon="search"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
+      <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details></v-text-field>
     </v-card-title>
-  <v-data-table
-  :headers="headers"
-  :items="homeowners"
-  :loading="loading"
-  :search="search"
-  class="elevation-1">
-    <v-progress-linear v-slot:progress color="blue" indeterminate></v-progress-linear>
-    <template v-slot:items="props">
-      <td class="text-xs-center">{{ props.item.id }}</td>
-      <td>{{ props.item.name }}</td>
-      <td>{{ props.item.email }}</td>
-      <td>{{ props.item.location }}</td>
-    </template>
-  </v-data-table>
-</v-card>
+    <v-data-table
+      :headers="headers"
+      :items="homeowners"
+      :loading="loading"
+      :search="search"
+      class="elevation-1"
+    >
+      <v-progress-linear v-slot:progress color="blue" indeterminate></v-progress-linear>
+      <template v-slot:items="props">
+        <td class="text-xs-center">{{ props.item.id }}</td>
+        <td>{{ props.item.name }}</td>
+        <td>{{ props.item.email }}</td>
+        <td>{{ props.item.location }}</td>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 
 <script>
 export default {
-    name: 'HealthFacilities',
-    data: () => ({
-        search: '',
-      loading: true,
-      headers: [
-        { text: 'ID', value: 'id' },
-        { text: 'Name', value: 'name' },
-        { text: 'E-mail', value: 'email' },
-        { text: 'Location', value: 'location'}
-      ],
-      homeowners: [],
-    }),
-}
+  name: "HealthFacilities",
+  data: () => ({
+    search: "",
+    loading: true,
+    headers: [
+      { text: "ID", value: "id" },
+      { text: "Name", value: "name" },
+      { text: "E-mail", value: "email" },
+      { text: "Location", value: "location" }
+    ],
+    homeowners: []
+  })
+};
 </script>
